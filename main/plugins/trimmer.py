1fe28bdc283410cf7c4bca9a302c3cf5a91d34e7
Codaangi <121353390+Jegaa1@users.noreply.github.com>
2024-12-09 20:11:25 +0530

    Update trimmer.py

--- main/plugins/trimmer.py
+++ main/plugins/trimmer.py
@@ -1,19 +1,4 @@
-#  This file is part of the VIDEOconvertor distribution.
-#  Copyright (c) 2021 vasusen-code ; All rights reserved. 
-#
-#  This program is free software: you can redistribute it and/or modify
-#  it under the terms of the GNU General Public License as published by
-#  the Free Software Foundation, version 3.
-#
-#  This program is distributed in the hope that it will be useful, but
-#  WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-#  General Public License for more details.
-#
-#  License can be found in < https://github.com/vasusen-code/VIDEOconvertor/blob/public/LICENSE> .
-
 import time, os
-
 from datetime import datetime as dt
 from telethon import events
 from telethon.errors.rpcerrorlist import MessageNotModifiedError
@@ -21,19 +6,19 @@ from telethon.tl.types import DocumentAttributeVideo
 from ethon.telefunc import fast_download, fast_upload
 from ethon.pyfunc import video_metadata, bash
 from ethon.pyutils import rename
-
 from .. import Drone, BOT_UN
-
 from LOCAL.localisation import SUPPORT_LINK, JPG, JPG2, JPG3
 
 async def trim(event, msg, st, et):
     Drone = event.client
-    edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=msg.id)
+    edit = await Drone.send_message(event.chat_id, "Trying to process.", reply_to=(link unavailable))
     new_name = "Asuran_" + dt.now().isoformat("_", "seconds")
+
     if hasattr(msg.media, "document"):
         file = msg.media.document
     else:
         file = msg.media
+
     mime = msg.file.mime_type
     if 'mp4' in mime:
         name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
@@ -42,21 +27,23 @@ async def trim(event, msg, st, et):
         name = "media_" + dt.now().isoformat("_", "seconds") + ".mp4"
         out = new_name + ".mp4"
     elif 'x-matroska' in mime:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv" 
-        out = new_name + ".mkv"       
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".mkv"
+        out = new_name + ".mkv"
     elif 'webm' in mime:
-        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm" 
+        name = "media_" + dt.now().isoformat("_", "seconds") + ".webm"
         out = new_name + ".webm"
     else:
         name = msg.file.name
         ext = (name.split("."))[1]
         out = new_name + ext
+
     DT = time.time()
     try:
         await fast_download(name, file, Drone, edit, DT, "**DOWNLOADING:**")
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while downloading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False) 
+        return await edit.edit(f"An error occured while downloading.\n\nContact SUPPORT", link_preview=False)
+
     try:
         await edit.edit("Trimming.")
         bash(f'ffmpeg -i {name} -ss {st} -to {et} -acodec copy -vcodec copy {out}')
@@ -64,11 +51,16 @@ async def trim(event, msg, st, et):
         rename(out, out2)
     except Exception as e:
         print(e)
-        return await edit.edit(f"An error occured while trimming!\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+        return await edit.edit(f"An error occured while trimming!\n\nContact SUPPORT", link_preview=False)
+
     UT = time.time()
     text = f""
+
     try:
         metadata = video_metadata(out2)
+        if metadata is None:
+            await edit.edit("Error: unable to retrieve video metadata.")
+            return
         width = metadata["width"]
         height = metadata["height"]
         duration = metadata["duration"]
@@ -81,11 +73,9 @@ async def trim(event, msg, st, et):
             await Drone.send_file(event.chat_id, uploader, caption=text, thumb=JPG, force_document=True)
         except Exception as e:
             print(e)
-            return await edit.edit(f"An error occured while uploading.\n\nContact [SUPPORT]({SUPPORT_LINK})", link_preview=False)
+            return await edit.edit(f"An error occured while uploading.\n\nContact SUPPORT", link_preview=False)
+
     await edit.delete()
     os.remove(name)
-    os.remove(out2)
-      
-      
-      
+    os.remove(out2)      
       
